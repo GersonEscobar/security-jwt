b714322ffcb92a7c58bd037d9c143457e0d401dc
GersonEscobar99 <gescobara3@miumg.edu.gt>
2024-09-27 00:58:44 -0600

    subiendo a prod

--- src/main/java/proyecto/dos/security_jwt/SecurityJwtApplication.java
+++ src/main/java/proyecto/dos/security_jwt/SecurityJwtApplication.java
@@ -2,12 +2,17 @@ package proyecto.dos.security_jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SecurityJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityJwtApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecurityJwtApplication.class);
 	}
 
 }
